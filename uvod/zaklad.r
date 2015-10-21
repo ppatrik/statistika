7d543b64e28758d928ab3fe157de9267e3678726
ppatrik <patrik.pekarcik@upjs.sk>
2015-10-21 10:08:53 +0200

    Create zaklad.r

---
+++ uvod/zaklad.r
@@ -0,0 +1,24 @@
+x=c(1,2,6); x
+1+1
+# vypis druhy prvok
+x[2]
+# vypis druhy az treti prvok
+x[2:3]
+# vypis okrem druheho prvku
+x[-2]
+# dlzka vektora x
+length(x)
+# zoradenie vektora
+sort(x, decreasing = T)
+# vytvorenie rozsahu
+z=seq(length=10, from=-5, by=0.5); z[]
+z=seq(length=10, from=-5, to=5); z[]
+# opakovanie hodnot
+rep(1:5, each=2)
+rep(1:5, times=2)
+# generovanie nahodnych cisel
+x=runif(2, 2, 5); x
+# generovanie celych cisel
+sample(2:10, 20, replace=T)
+
+
